10c032357abf91c61d7e1a321b3de8e97b35c796
jommy belinda <jbelinda@student.21-school.ru>
2019-09-29 17:50:49 +0300

    BUFF_SIZE adjustment

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,21 @@
 /*   By: jbelinda <jbelinda@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/28 05:24:25 by jbelinda          #+#    #+#             */
-/*   Updated: 2019/09/28 05:28:13 by jbelinda         ###   ########.fr       */
+/*   Updated: 2019/09/29 16:22:04 by jbelinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <limits.h>
+
+# ifndef BUFF_SIZE
+#  define BUFF_SIZE (4096)
+# elif BUFF_SIZE > SSIZE_MAX
+#  undef BUFF_SIZE
+#  define BUFF_SIZE (4096)
+# endif
 
 int	get_next_line(const int fd, char **line);
 
